551a44768400c8832a309daac2e6bc7d380d8a8c
Bruhadesh Varma <kbruha5@gmail.com>
2026-01-30 23:31:56 +0530

    Time: 639 ms (69.93%), Space: 32.1 MB (17.29%) - LeetHub

---
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minimumTime(self, time: List[int], totalTrips: int) -> int:
+        left =1
+        right = min(time)*totalTrips
+        while left < right:
+            mid = (left+right)//2
+            trips =sum(mid //  t for t in time)
+            
+            if trips >= totalTrips:
+                right = mid
+            else:
+                left = mid+1
+        return left
